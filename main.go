b4fcce6bce24a230f47882156e4862fb04093ccc
Amit <amit@192.168.1.2>
2022-11-19 15:25:23 +0530

    seaching and sorting

--- main.go
+++ main.go
@@ -20,7 +20,26 @@ package main
 // 	out := make()
 // }
 
+func sort(asse , des []int) []int {
+	asselen := len(asse)
+	deslen := len(des)
+	ans := make([]int,asselen)
+	if asselen && deslen < 1 {
+		return []int
+	}
+
+	for i:= 0, i < asselen; i++{
+		for j := 0; j < deslen; j++ {
+			if asse[i] < des[j]{
+				ans = append(ans, )
+			}
+		}
+	}
+}
+
 func main() {
 	arr := []int{1, 2, 3, 4, 5}
 	arr2 := []int{10, 9, 8, 7, 3}
+
+
 }
---
+++ searchandsort/sqrt.go
@@ -0,0 +1,3 @@
+package searchandsort
+
+func Sqrt()
